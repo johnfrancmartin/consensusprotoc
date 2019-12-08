95998555f2a7b28390b9951392d15dcb98aee8b0
John Martin <john.franc.martin@gmail.com>
2019-12-08 04:43:07 -0500

    Python3

--- Main.sh
+++ Main.sh
@@ -3,8 +3,8 @@
 killall python
 killall python3
 source ./venv/bin/activate;
-python ./CreateBLS.py 2 4;
-python ./ReplicaMain.py 4 1 &
-python ./ReplicaMain.py 4 2 &
-python ./ReplicaMain.py 4 3 &
-python ./ReplicaMain.py 4 4;
+python3 ./CreateBLS.py 2 4;
+python3 ./ReplicaMain.py 4 1 &
+python3 ./ReplicaMain.py 4 2 &
+python3 ./ReplicaMain.py 4 3 &
+python3 ./ReplicaMain.py 4 4;
