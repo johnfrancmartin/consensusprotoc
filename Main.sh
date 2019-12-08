5a7e62894d16a9bcdb8cdeeaaac0443fcc133cff
John Martin <john.franc.martin@gmail.com>
2019-12-08 04:51:20 -0500

    boo

--- Main.sh
+++ Main.sh
@@ -1,6 +1,9 @@
+#!/usr/bin/env bash
+
 killall python
 killall python3
 source ./venv/bin/activate;
+pip list;
 python3 ./CreateBLS.py 2 4;
 python3 ./ReplicaMain.py 4 1 &
 python3 ./ReplicaMain.py 4 2 &
