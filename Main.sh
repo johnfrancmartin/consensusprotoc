2c4be30fb4febcda25f50f2e890730cd5d104e96
John Martin <john.franc.martin@gmail.com>
2019-12-08 05:53:13 -0500

    ERR

--- Main.sh
+++ Main.sh
@@ -14,7 +14,8 @@ python3 ./CreateBLS.py 2 4;
 #python3 ./ReplicaMain.py 4 4 &
 
 n = 4
-for i in {1..$n+1}; do
+top = $n+1
+for i in {1..$top}; do
     echo $i
     python3 ./ReplicaMain.py $n $i &
     sleep 1
