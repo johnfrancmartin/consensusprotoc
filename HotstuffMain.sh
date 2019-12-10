3ada8cc23b4f8d1d9ddfab9527956cd7d5437e74
John Martin <john.franc.martin@gmail.com>
2019-12-10 18:24:42 -0500

    Getting closer to a rushed hotstuff

--- HotstuffMain.sh
+++ HotstuffMain.sh
@@ -3,16 +3,16 @@
 killall python3;
 source ./venv/bin/activate;
 
-n=32
-python3 ./GenerateKeys.py 32
+n=4
+python3 ./GenerateKeys.py 4
 
-python3 ./HotstuffReplicaMain.py 4 1 &
+python3 ./HotstuffReplicaMain.py ${n} 1 &
 sleep 1
-python3 ./HotstuffReplicaMain.py 4 2 &
+python3 ./HotstuffReplicaMain.py ${n} 2 &
 sleep 1
-python3 ./HotstuffReplicaMain.py 4 3 &
+python3 ./HotstuffReplicaMain.py ${n} 3 &
 sleep 1
-python3 ./HotstuffReplicaMain.py 4 4 &
+python3 ./HotstuffReplicaMain.py ${n} 4 &
 sleep 1
 python3 ./ClientMain.py ${n} 1 512;
 
