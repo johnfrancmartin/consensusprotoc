d98002a36270e0e7844dc0f5dd86c89902342f38
John Martin <john.franc.martin@gmail.com>
2019-12-10 17:03:26 -0500

    32 Reps, VM3

--- Main.sh
+++ Main.sh
@@ -5,10 +5,8 @@ source ./venv/bin/activate;
 
 n=32
 python3 ./GenerateKeys.py 32
-for i in {1..32}
-do
-    echo $i
-    task python3 replicanetwork.py 32 $i &
+for i in {1..32}; do
+    python3 replicanetwork.py 32 $i &
     sleep 1
 done
 
