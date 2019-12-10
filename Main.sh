79666c36d1b57443938cd5bcb39ba33201902ac5
John Martin <john.franc.martin@gmail.com>
2019-12-10 17:08:24 -0500

    32 Reps, VM3

--- Main.sh
+++ Main.sh
@@ -5,9 +5,9 @@ source ./venv/bin/activate;
 
 n=32
 python3 ./GenerateKeys.py 32
-for i in {1..${n}}
+for i in {1..32};
 do
-    echo $i
+    echo ${i}
     python3 ./ReplicaMain.py ${n} ${i} &
     sleep 1
 done
