b574e90bbb80f4dadbfa4b89b03a8c1999371c41
John Martin <john.franc.martin@gmail.com>
2019-12-10 16:54:02 -0500

    32 Reps, VM3

--- Main.sh
+++ Main.sh
@@ -4,15 +4,15 @@ killall python3;
 source ./venv/bin/activate;
 
 n=32
-python3 ./GenerateKeys.py $n
-for i in {0..$n}
+python3 ./GenerateKeys.py ${n}
+for i in {0..${n}}
 do
-    echo $i
-    python3 replicanetwork.py $n $i &
+    echo ${i}
+    python3 replicanetwork.py ${n} ${i} &
     sleep 1
 done
 
-python3 ./ClientMain.py $n 1 64;
+python3 ./ClientMain.py ${n} 1 64;
 
 #n = 4
 #for i in {1..4}; do
