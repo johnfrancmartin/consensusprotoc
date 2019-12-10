d340bd347fb58b940dbcab429a90c3f72264a413
John Martin <john.franc.martin@gmail.com>
2019-12-10 17:06:06 -0500

    32 Reps, VM3

--- Main.sh
+++ Main.sh
@@ -5,11 +5,11 @@ source ./venv/bin/activate;
 
 n=32
 python3 ./GenerateKeys.py 32
-for i in {1..32}; do
-    python3 replicanetwork.py 32 $i &
+for i in {0..$n}; do
+    echo $i
+    python3 ./ReplicaMain.py $n $i &
     sleep 1
 done
-
 python3 ./ClientMain.py 32 1 64;
 
 #n = 4
