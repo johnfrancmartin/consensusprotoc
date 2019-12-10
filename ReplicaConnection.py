c01bac0a15f4bf6661131d7210e3a476e683c14b
John Martin <john.franc.martin@gmail.com>
2019-12-10 16:26:28 -0500

    1024

--- ReplicaConnection.py
+++ ReplicaConnection.py
@@ -424,7 +424,8 @@ class ReplicaConnection:
                 # print("ERROR RECEIVING MSG", e)
                 pass
         try:
-            print("MESSAGE LENGTH", msg_len, flush=True)
+            if self.print:
+                print("MESSAGE LENGTH", msg_len, flush=True)
             whole_msg = sock.recv(msg_len)
             prototype.ParseFromString(whole_msg)
             sock.setblocking(0)
