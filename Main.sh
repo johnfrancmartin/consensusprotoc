836809e713195898d3d1bf72d765a28bd1763b45
John Martin <john.franc.martin@gmail.com>
2019-12-10 20:19:48 -0500

    Back to Flex

--- Main.sh
+++ Main.sh
@@ -3,9 +3,9 @@
 killall python3;
 source ./venv/bin/activate;
 
-n=32
-python3 ./GenerateKeys.py 32
-for i in {1..32};
+n=4
+python3 ./GenerateKeys.py 4
+for i in {1..4};
 do
     echo ${i}
     python3 ./ReplicaMain.py ${n} ${i} &
